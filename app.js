13ccb21558ea7aab0dc5578f0f6f5c9956729306
Kartik Narang <narangk007@gmail.com>
2023-10-07 11:04:24 +0530

    Starting Mongoose Implementation

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
-const User = require('./models/user');
+const mongoose = require('mongoose');
+
+// const User = require('./models/user');
 
 const app = express();
 
@@ -18,20 +19,22 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  User.findById('651a9a13c56bd573acc5a652')
-    .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById('651a9a13c56bd573acc5a652')
+//     .then(user => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
+mongoose.connect('mongodb+srv://narangk007:hima0007@cluster0.vxcp2f3.mongodb.net/shop?retryWrites=true&w=majority').then(result=>{
   app.listen(3000);
-});
+}).catch(err=>{
+  console.log(err);
+})
--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 const mongodb = require('mongodb');
-const getDb = require('../util/database').getDb;
+// const getDb = require('../util/database').getDb;
 
 const ObjectId = mongodb.ObjectId;
 
