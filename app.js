c5ee77253734dd489e63720ae1b48bcf9ae7679c
Kartik Narang <narangk007@gmail.com>
2023-10-07 11:36:26 +0530

    Added User Model Using Mongoose

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
 const mongoose = require('mongoose');
 
-// const User = require('./models/user');
+const User = require('./models/user');
 
 const app = express();
 
@@ -19,14 +19,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findById('651a9a13c56bd573acc5a652')
-//     .then(user => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById('6520f41df01db475e564839d')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -34,6 +34,18 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 mongoose.connect('mongodb+srv://narangk007:hima0007@cluster0.vxcp2f3.mongodb.net/shop?retryWrites=true&w=majority').then(result=>{
+  User.findOne().then(user=>{
+    if(!user){
+      const user = new User({
+        name: "Kartik Narang",
+        email: "narangk007@gmail.com",
+        cart:{
+          items: []
+        }
+      });
+      user.save();
+    }
+  })
   app.listen(3000);
 }).catch(err=>{
   console.log(err);
